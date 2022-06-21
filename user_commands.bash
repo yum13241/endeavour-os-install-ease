e5331df8168fb481c4d97285d8b1ba178452d967
Not Yummy <anarchyforall123@gmail.com>
2022-06-21 15:12:55 +0300

    Update user_commands.bash
    Fixed breaking bugs.

--- user_commands.bash
+++ user_commands.bash
@@ -7,7 +7,9 @@ pacman -Syu --no-confirm opendoas
 #Add chaotic AUR to save ourselves in the long run.
 pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
 pacman-key --lsign-key FBA220DFC880C036
-pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+pacman -U --noconfirm 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 #Crude hack to add Chaotic AUR to repo list.
 echo [chaotic-aur] >> /etc/pacman.conf
 echo Include = /etc/pacman.d/chaotic-mirrorlist >> /etc/pacman.conf
+pacman -Syu opendoas-sudo
+pacman -Syu pikaur-git
